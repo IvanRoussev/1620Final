652cc32f7a2cb5890bb1600b647ef3b70ccd168c
Ivan <ivan.roussev12@gmail.com>
2021-12-10 12:23:27 -0800

    made a function and an event listerner which changes between dark and ligh mode still in a work in progress not functional yet

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,23 @@
-function swap(event) {
+function swap(evt) {
     nodelist = document.getElementsByTagName("*")
 
+    for (let i = 0; i < nodelist.length; i++) {
+        classlist = nodelist[i].classlist
+        if (!classlist.contain('--light-mode')) {
+            classlist.add('--light-mode')
+            classlist.remove('--dark-mode') }
+        else {
+            classlist.remove('--light-mode')
+            classlist.add('--dark-mode')
+        }
+        }
+  }
+
+
+const toggle_button = document.querySelector(".toggle")
+    evt.swap()
+  
+toggle_button.addEventListener("click", swap)
 
 
 
@@ -9,4 +26,3 @@ function swap(event) {
 
 
 
-}
