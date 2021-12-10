d583806d22207e88e23609aabbb1bf8cc3efa06f
Ivan <ivan.roussev12@gmail.com>
2021-12-10 13:15:46 -0800

    Tried my best with the functions cant get them to run but hopefully they seem good :)

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,42 @@
-function swap(evt) {
-    nodelist = document.getElementsByTagName("*")
 
-    for (let i = 0; i < nodelist.length; i++) {
-        classlist = nodelist[i].classlist
-        if (!classlist.contain('--light-mode')) {
-            classlist.add('--light-mode')
-            classlist.remove('--dark-mode') }
-        else {
-            classlist.remove('--light-mode')
-            classlist.add('--dark-mode')
+
+let dark_theme = document.querySelector(".toggle");
+
+
+dark_theme.addEventListener("click", (e) => {
+    let classlist = document.querySelector(":root")
+    
+        for (let i = 0; i < nodelist.length; i++) {
+            e.classlist = nodelist[i].classlist
+            if (!classlist.contain('--light-theme')) {
+                classlist.add('--light-theme')
+                classlist.remove('--dark-theme') }
+            else {
+                classlist.remove('--light-theme')
+                classlist.add('--dark-theme')
+            }
+            }
+      }
+);
+
+
+
+
+function createformonInput() {
+    let my_form = document("div");
+    my_form.className = "contact";
+    let form_text = document.createTextNode(`<label for="msg">Message</label>`);
+    my_form.appendChild(form_text);
+    my_form.addEventListener("click", (e) => {
+    e.preventDefault();
+    inputform = e.target.textContent;
+    renderView(contactList[i]); 
         }
-        }
+    );
+    return index_div;
   }
 
-
-const toggle_button = document.querySelector(".toggle")
-    evt.swap()
-  
-toggle_button.addEventListener("click", swap)
-
-
-
-
-
-
+  console.log(createformonInput)
 
 
 
